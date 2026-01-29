ae33615956baec99a41876d4b905b3dbad7ed249
efreddi <eleonora.freddi@telenor.com>
2026-01-29 15:18:13 +0100

    update instructions
    further update in instructions pages

--- 2025_new_design_and_experiment/experiment_code/social_exc_2025/social_exc_2025/dictator/__init__.py
+++ 2025_new_design_and_experiment/experiment_code/social_exc_2025/social_exc_2025/dictator/__init__.py
@@ -42,11 +42,11 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     # One field per round
-    choice_round_1 = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs do you want to send to the other participant?")
-    choice_round_2a = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs do you want to send to the other participant?")
-    choice_round_2b = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs do you want to send to the other participant?")
-    choice_round_3a = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs do you want to send to the other participant?")
-    choice_round_3b = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs do you want to send to the other participant?")
+    choice_round_1 = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs out of 20 do you want to send to the other participant?")
+    choice_round_2a = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs out of 20 do you want to send to the other participant?")
+    choice_round_2b = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs out of 20 do you want to send to the other participant?")
+    choice_round_3a = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs out of 20 do you want to send to the other participant?")
+    choice_round_3b = models.CurrencyField(min=0, max=C.ENDOWMENT, label="How many EMUs out of 20 do you want to send to the other participant?")
     #choice_round_4 = models.CurrencyField(min=0, max=C.ENDOWMENT, label="If the recipient is from your group and did easy / hard task")
     #choice_round_5 = models.CurrencyField(min=0, max=C.ENDOWMENT, label="If the recipient is from the other group and did easy / hard task")
 
